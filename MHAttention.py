903b7495556f03dce414961c9a3d82a27de2a2ec
rhiannonyy <58084313+rhiannonyy@users.noreply.github.com>
2020-11-30 21:46:39 -0500

    Update MHAttention.py

--- MHAttention.py
+++ MHAttention.py
@@ -1,6 +1,23 @@
 import tensorflow as tf
 import numpy as np
 
+def get_EF(input_size, dim, method="learnable", head_dim=None, bias=True):
+    """
+    Retuns the E or F matrix, initialized via xavier(Glorot) initialization.
+    This is the recommended way to do it according to the authors of the paper.
+    Includes a method for convolution, as well as a method for no additional params.
+    """
+    # assert method == "learnable" or method == "convolution" or method == "no_params", "The method flag needs to be either 'learnable', 'convolution', or 'no_params'!"
+    # if method == "convolution":
+    #     conv = tf.keras.layers.Conv1D(head_dim, kernel_size=int(input_size/dim), strides=int(input_size/dim))
+    #     return conv
+    # if method == "no_params":
+    #     return tf.keras.backend.random_normal(shape=(input_size, dim), mean=0.0, std=1/dim)
+    dense1 = tf.keras.layers.Dense(dim, use_bias=True)
+    dense1 = tf.Variable(tf.initializers.GlorotUniform()(shape=dense1.get_weights()))
+    return dense1
+
+
 class MHAttention(tf.keras.layers.Layer):
     def __init__(self, input_size, dim, channels, dim_k, nhead, dropout, checkpoint_level,
             parameter_sharing, E_proj, F_proj, full_attention, causal_mask, w_o_intermediate_dim=None, decoder_mode=False, method="learnable"):
@@ -28,14 +45,14 @@ class MHAttention(tf.keras.layers.Layer):
                     F_proj = get_EF(input_size, dim_k, method, dim)
                 attn = LinearAttentionHead(dim, dropout, E_proj, F_proj, causal_mask, full_attention)
                 self.heads.add(attn)
-                self.to_q.add(Dense(dim, use_bias=False))
-                self.to_k.add(Dense(dim, use_bias=False))
-                self.to_v.add(Dense(dim, use_bias=False))
+                self.to_q.add(tf.keras.layers.Dense(dim, use_bias=False))
+                self.to_k.add(tf.keras.layers.Dense(dim, use_bias=False))
+                self.to_v.add(tf.keras.layers.Dense(dim, use_bias=False))
             if w_o_intermediate_dim is None:
-                self.w_o = Dense(channels)
+                self.w_o = tf.keras.layers.Dense(channels)
             else:
-                self.w_o_1 = Dense(w_o_intermediate_dim)
-                self.w_o_2 = Dense(channels)
+                self.w_o_1 = tf.keras.layers.Dense(w_o_intermediate_dim)
+                self.w_o_2 = tf.keras.layers.Dense(channels)
             self.mh_dropout = tf.nn.Dropout(dropout)
 
         def call(self, tensor, **kwargs):
